ec01d898a3c930f2ac318d7a8e7be09626762d12
superboy-sakurama <trimariyonohu@gmail.com>
2026-06-10 17:44:45 +0700

    Add 'HUTANG' to PaymentMethod and 'UNPAID' to status

--- src/types.ts
+++ src/types.ts
@@ -28,7 +28,7 @@ export interface Customer {
   created_at?: string;
 }
 
-export type PaymentMethod = 'CASH' | 'DEBIT' | 'QRIS' | 'TRANSFER';
+export type PaymentMethod = 'CASH' | 'DEBIT' | 'QRIS' | 'TRANSFER' | 'HUTANG';
 
 export interface Transaction {
   id: string; // UUID
@@ -37,7 +37,7 @@ export interface Transaction {
   payment_method: PaymentMethod;
   customer_id: string | null;
   user_id: string; // ID Kasir
-  status: 'COMPLETED' | 'REFUNDED';
+  status: 'COMPLETED' | 'REFUNDED' | 'UNPAID';
   created_at?: string;
 }
 
@@ -61,4 +61,3 @@ export interface AppSettings {
   app_name: string;
   logo_url: string;
 }
-
